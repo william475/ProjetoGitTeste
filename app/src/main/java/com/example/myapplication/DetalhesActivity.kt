846808ca43c0898826f2c71560c35304b87449a2
william_wander <williamwander2009@hotmai.com>
2026-04-17 15:37:24 -0300

    Criado detalhes activity

--- app/src/main/java/com/example/myapplication/DetalhesActivity.kt
+++ app/src/main/java/com/example/myapplication/DetalhesActivity.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.B
 
 class DetalhesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
